2fa7517c479e0c25f8092e1a16d9ebe38057d297
Amanda Ericson <eri24017@byui.edu>
2025-05-06 12:23:38 -0600

    more

--- mission/mission.js
+++ mission/mission.js
@@ -1,6 +1,17 @@
 const themeSelect = document.getElementById('theme-select');
 const body = document.body;
+const logo = document.querySelector('footer img');
 
-themeSelect.addEventListener('change', function () {
-    body.className = this.value;
-});
+function changeTheme() {
+    const selectedTheme = themeSelect.value;
+
+    if (selectedTheme === 'dark') {
+        body.classList.add('dark');
+        logo.src = 'byui-logo_white.webp'; // Replace with actual white logo file path
+    } else {
+        body.classList.remove('dark');
+        logo.src = 'byui-logo_blue.webp';
+    }
+}
+
+themeSelect.addEventListener('change', changeTheme);
